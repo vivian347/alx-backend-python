8b41af7dbd7c9824563ca4d2f95182c2eedf836e
vivian347 <vkinyua001@gmail.com>
2023-04-22 16:45:52 +0300

    Asyncio

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,15 +2,12 @@
 
 """task_wait_random"""
 
-from typing import TypeVar
 import asyncio
+from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-# Define a type variable T that can only be bound to an instance of asyncio.Task
-T = TypeVar('T', bound=asyncio.Task)
 
-
-def task_wait_random(max_delay: int) -> T:
+def task_wait_random(max_delay: int) -> Task:
     """takes in max_delay and returns a asyncio.Task"""
     task = asyncio.create_task(wait_random(max_delay))
     return task
